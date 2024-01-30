e2d4bdcb3311b391dbae7acc0c1ddf4847b5d7c2
nurjolone <nnn@nnns-MacBook-Pro.local>
2024-01-30 16:22:25 -0600

    last update

--- src/test/java/com/openMRS/pages/LoginOpenMRS.java
+++ src/test/java/com/openMRS/pages/LoginOpenMRS.java
@@ -1,9 +1,14 @@
 package com.openMRS.pages;
 
+import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utils.BrowserUtils;
+import utils.DriverHelper;
 
 public class LoginOpenMRS {
     public LoginOpenMRS (WebDriver driver){
@@ -11,12 +16,32 @@ public class LoginOpenMRS {
 
     }
     @FindBy(css ="input[id='username']")
-    WebElement username;
+    WebElement userName;
     @FindBy(css = "input[id='password']")
     WebElement password;
 
     @FindBy (css = "li[id='Registration Desk']")
     WebElement registrationBtn;
+    @FindBy(css = "input[id='loginButton']")
+    WebElement loginBtn;
+
+    public void userNameAndPasswordFiled(String userName, String password){
+        this.userName.sendKeys(userName);
+        this.password.sendKeys(password);
+    }
+    public void registrationAndLoginBtn(){
+        this.registrationBtn.click();
+        this.loginBtn.click();
+    }
+
+
+//    public void setDriver(){
+//        driver = DriverHelper.getDriver();
+//    }
+
+
+
+
 
 
 
--- src/test/resources/openMrs/OpenMRS.feature
+++ src/test/resources/openMrs/OpenMRS.feature
@@ -1,5 +1,5 @@
 Feature:  OpenMrs
-  Scenario Outline:  OpenMrs
+  Scenario Outline:Positive path OpenMRS
     Given user in OpenMrs page provides '<username>' and '<password>'
     Then user clicks Registration Desk and clciks loginBtn
     And user validates that  user in Home page by validating  '<header>'
@@ -8,6 +8,9 @@ Feature:  OpenMrs
     |username|Admin|
     |password|Admin123|
     |header   |Logged in as Super User (admin) at Registration Desk.|
+
+
+
 
 
 
