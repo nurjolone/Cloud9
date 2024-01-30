cfcf3c8fb72bf1d43d2456943203e5ee0898280c
nurjolone <nnn@nnns-MacBook-Pro.local>
2024-01-30 12:36:49 -0600

    updates

--- src/test/java/com/openMRS/pages/LoginOpenMRS.java
+++ src/test/java/com/openMRS/pages/LoginOpenMRS.java
@@ -1,5 +1,23 @@
 package com.openMRS.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginOpenMRS {
+    public LoginOpenMRS (WebDriver driver){
+        PageFactory.initElements(driver,this);
+
+    }
+    @FindBy(css ="input[id='username']")
+    WebElement username;
+    @FindBy(css = "input[id='password']")
+    WebElement password;
+
+    @FindBy (css = "li[id='Registration Desk']")
+    WebElement registrationBtn;
+
+
 
 }
